edfaeda57205fd44776fb90cc95779ba16eacb0a
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-04 18:26:59 +0100

    Add project files.

---
+++ KillBind/BasePlugin.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalCompanyInputUtils.Api;
+using UnityEngine.InputSystem;
+
+namespace KillBindNS
+{
+    public class KillBind : LcInputActions
+    {
+        [InputAction("<Keyboard>/k", Name = "Suicide", ActionType = InputActionType.Button)]
+        public InputAction ExplodeKey { get; set; }
+    }
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.HardDependency)]
+    public class BasePlugin : BaseUnityPlugin
+    {
+        //Mod Defining Vars
+        private const string modGUID = "Confusified.KillBind";
+        private const string modName = "Kill Bind";
+        private const string modVersion = "1.2.0";
+        private readonly Harmony _harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+
+        //Mod Config Vars
+        public ConfigFile Configuration = new ConfigFile(Utility.CombinePaths(Paths.ConfigPath + "\\" + modGUID.Replace(".", "\\") + ".cfg"), false);
+        public static ConfigEntry<bool> ModEnabled;
+        public static ConfigEntry<int> DeathCause;
+        public static ConfigEntry<int> HeadType;
+
+        //Mod Non-Config Vars
+        public static KillBind InputActionInstance = new KillBind();
+
+        //Mod Functions
+        public void Awake()
+        {
+            SetModConfig();
+            _harmony.PatchAll();
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls = this.Logger;
+            mls.LogInfo($"{modName} {modVersion} has loaded");
+        }
+
+        public void SetModConfig()
+        {
+            ModEnabled = Configuration.Bind<bool>("Global Settings", "Enabled", true, "Toggle the mod");
+            DeathCause = Configuration.Bind<int>("Mod Settings", "DeathType", 0, "Decide what cause of death you will have when pressing your kill bind.\n" +
+                "There are currently 13 death causes in the game:\n" +
+                "(0) Unknown (Default)\n(1) Bludgeoning\n(2) Gravity\n(3) Blast\n(4) Strangulation\n(5) Suffocation\n(6) Mauling\n(7) Gunshots\n(8) Crushing\n(9) Drowning\n(10) Abandoned\n(11) Electrocution\n(12) Kicking" +
+                "\n\nDeath types (1)Bludgeoning, (6)Mauling and (7)Gunshots will cover the body with blood."
+                );
+            HeadType = Configuration.Bind<int>("Mod Settings", "HeadType", 1, "Decide what will happen with your head.\n" +
+                "There are currently 3 head types in the game:\n" +
+                "(0) Default - Your head will stay on your body\n" +
+                "(1) Decapitate (Default) - Your head will be blown off of your body\n" +
+                "(2) Coilhead - Your head will be replaced with a coil"
+                );
+        }
+    }
+}
---
+++ KillBind/Binds/KillBindPatch.cs
@@ -0,0 +1,25 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using KillBindNS;
+using System;
+using UnityEngine;
+
+namespace KillBind.Binds
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    public class KillBindPatch
+    {
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        public static void onPressKill(PlayerControllerB __instance)
+        {
+            if (BasePlugin.ModEnabled.Value && BasePlugin.InputActionInstance.ExplodeKey.triggered && __instance == GameNetworkManager.Instance.localPlayerController && !__instance.isPlayerDead && !__instance.isTypingChat && !HUDManager.Instance.typingIndicator.enabled && (!UnityEngine.Object.FindObjectOfType<Terminal>().terminalInUse && !__instance.inTerminalMenu))
+            {
+                if (BasePlugin.DeathCause.Value > Enum.GetValues(typeof(CauseOfDeath)).Length || BasePlugin.DeathCause.Value < 0) { BasePlugin.DeathCause.Value = 0; BasePlugin.mls.LogInfo("Your config for HeadType is invalid, defaulting to value 0"); } //If your choice is invalid, set to default (unknown death cause)
+                if (BasePlugin.HeadType.Value > 3 || BasePlugin.HeadType.Value < 0) { BasePlugin.HeadType.Value = 1; BasePlugin.mls.LogInfo("Your config for HeadType is invalid, defaulting to value 1"); } //If your choice is invalid, set to default (explode head)
+
+                __instance.KillPlayer(Vector3.zero, true, (CauseOfDeath)BasePlugin.DeathCause.Value, BasePlugin.HeadType.Value);
+            }
+        }
+    }
+}
