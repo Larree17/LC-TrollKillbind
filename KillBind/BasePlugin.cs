68957abb771b1008c5999f7749e3e2de4a17c0fd
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-07 20:28:20 +0100

    progress w/ using prefabs, it now loads and becomes visible

--- KillBind/BasePlugin.cs
+++ KillBind/BasePlugin.cs
@@ -7,7 +7,6 @@ using LethalCompanyInputUtils.Api;
 using UnityEngine.InputSystem;
 using System.Reflection;
 using System.IO;
-using KillBind.Patches;
 
 namespace KillBindNS
 {
@@ -29,25 +28,33 @@ namespace KillBindNS
         public static ManualLogSource mls;
 
         //Mod Config Vars
-        public ConfigFile Configuration = new ConfigFile(Utility.CombinePaths(Paths.ConfigPath + "\\" + modGUID.Replace(".", "\\") + ".cfg"), false);
+        private ConfigFile Configuration = new ConfigFile(Utility.CombinePaths(Paths.ConfigPath + "\\" + modGUID.Replace(".", "\\") + ".cfg"), false);
         public static ConfigEntry<bool> ModEnabled;
         public static ConfigEntry<int> DeathCause;
         public static ConfigEntry<int> HeadType;
 
         //Mod Non-Config Vars
         public static KillBind InputActionInstance = new KillBind();
-        public static AssetBundle ModMenu;
+        public static AssetBundle ModAssetBundle;
+        public static GameObject Menu1;
+        public static GameObject Menu2;
         //Mod Functions
         public void Awake()
         {
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
             mls = this.Logger;
-
-            ModMenu = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "killbindmenu"));
-            if (ModMenu == null)
+            ModAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "bundlekillbind"));
+            if (ModAssetBundle == null)
             {
-                mls.LogError("Error while trying to load prefab.");
+                mls.LogError("Error while trying to load AssetBundle.");
                 return;
+            }
+            Menu1 = ModAssetBundle.LoadAsset<GameObject>("Assets/KillBindMod/MenuToggle.prefab");
+            Menu2 = ModAssetBundle.LoadAsset<GameObject>("Assets/KillBindMod/MenuUI.prefab");
+
+            if (Menu1 == null || Menu2 == null)
+            {
+                mls.LogError("Error while trying to load a prefab.");
             }
 
             SetModConfig();
@@ -60,12 +67,10 @@ namespace KillBindNS
             ModEnabled = Configuration.Bind<bool>("Global Settings", "Enabled", true, "Toggle the mod");
             DeathCause = Configuration.Bind<int>("Mod Settings", "DeathType", 0, "Decide what cause of death you will have when pressing your kill bind.\n" +
                 "There are currently 13 death causes in the game:\n" +
-                "(0) Unknown (Default)\n(1) Bludgeoning\n(2) Gravity\n(3) Blast\n(4) Strangulation\n(5) Suffocation\n(6) Mauling\n(7) Gunshots\n(8) Crushing\n(9) Drowning\n(10) Abandoned\n(11) Electrocution\n(12) Kicking" +
-                "\n\nDeath types (1)Bludgeoning, (6)Mauling and (7)Gunshots will cover the body with blood."
-                );
+                "(0) Unknown (Default)\n(1) Bludgeoning\n(2) Gravity\n(3) Blast\n(4) Strangulation\n(5) Suffocation\n(6) Mauling\n(7) Gunshots\n(8) Crushing\n(9) Drowning\n(10) Abandoned\n(11) Electrocution\n(12) Kicking");
             HeadType = Configuration.Bind<int>("Mod Settings", "HeadType", 1, "Decide what will happen with your head.\n" +
                 "There are currently 3 head types in the game:\n" +
-                "(0) Default - Your head will stay on your body\n" +
+                "(0) Normal - Your head will stay on your body\n" +
                 "(1) Decapitate (Default) - Your head will be blown off of your body\n" +
                 "(2) Coilhead - Your head will be replaced with a coil"
                 );
---
+++ KillBind/Patches/MenuCode.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using KillBindNS;
+using UnityEngine;
+
+namespace KillBind.Patches
+{
+    [HarmonyPatch(typeof(MenuManager))]
+    public class MenuCode
+    {
+        public static GameObject MenuToggle; //Toggle button that will be found in settings panel
+        public static GameObject SettingsUI; //Settings UI of this mod
+        //WHAT
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        public static void onAwake()
+        {
+            //Toggle Button Creation
+            MenuToggle = Object.Instantiate<GameObject>(BasePlugin.Menu1);
+            Object.DontDestroyOnLoad(MenuToggle); //Do not destroy on scene change
+            MenuToggle.SetActive(false);
+
+            //Settings UI Creation
+            SettingsUI = Object.Instantiate<GameObject>(BasePlugin.Menu2);
+            Object.DontDestroyOnLoad(SettingsUI); //Do not destroy on scene change
+            SettingsUI.SetActive(false);
+        }
+
+        [HarmonyPatch("EnableUIPanel")]
+        [HarmonyPostfix]
+        public static void onEnable(GameObject enablePanel)
+        {
+            if (enablePanel.name == "SettingsPanel")
+            {
+                MenuToggle.SetActive(true);
+            }
+        }
+
+        [HarmonyPatch("DisableUIPanel")]
+        [HarmonyPostfix]
+        public static void onDisable(GameObject enablePanel)
+        {
+            if (enablePanel.name == "SettingsPanel")
+            {
+                MenuToggle.SetActive(false);
+            }
+        }
+    }
+}
