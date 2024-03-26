6e9a686ed64cee2ae9a83f10c9d135e0e353337c
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-26 18:28:00 +0100

    button can function,  time to write the function!!!!!!!

--- KillBind/Patches/UIHandler.cs
+++ KillBind/Patches/UIHandler.cs
@@ -1,6 +1,8 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using static KillBind.Initialise;
 
 namespace KillBind.Patches
@@ -10,8 +12,9 @@ namespace KillBind.Patches
         //Memory
 
         private static GameObject mButton;
-        private static Transform mButtonTransform;
         private static TextMeshProUGUI mButtonTextComp;
+
+        private static GameObject mMenu;
 
         private static GameObject mSettingsPanel;
         private static Transform mSettingsPanelTransform;
@@ -21,7 +24,7 @@ namespace KillBind.Patches
         private static bool ExistsInMemory = false;
 
         private static GameObject TalkMode;
-        private static Transform TalkModeTransform;
+        private static GameObject MenuContainer;
 
         //Scene
 
@@ -30,10 +33,7 @@ namespace KillBind.Patches
         private static GameObject sceneSettingsPanel;
         private static Transform sceneSettingsPanelTransform;
 
-        private static Vector3 MainMenuPositionOffset = new Vector3(53.369f, 7.19f, 0);
-        private static Vector3 QuickMenuOffsetScale = new Vector3(0.05714f, 0.06875f, 0); //for easier changes (maybe do it better sometime soon, with screen res n stuff)
-        private static Vector3 QuickMenuPositionOffset = Vector3.Scale(MainMenuPositionOffset, QuickMenuOffsetScale);
-        private static Vector3 currentOffset;
+        private static Vector3 ButtonLocalPosition = new Vector3(9.0871f, 235.4723f, -4.7728f);
 
         /* Copy TalkMode (put in SettingsPanel)
          * Remove SettingsOption
@@ -48,16 +48,18 @@ namespace KillBind.Patches
             if (ExistsInMemory) { return; } //To avoid potential memory leaks
             if (IsInMainMenu())
             {
-                mSettingsPanel = GameObject.Find("Canvas").gameObject.transform.Find("MenuContainer").gameObject.transform.Find("SettingsPanel").gameObject;
+                MenuContainer = GameObject.Find("Canvas").gameObject.transform.Find("MenuContainer").gameObject;
             }
             else
             {
-                mSettingsPanel = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject.transform.Find("SettingsPanel").gameObject; //Shouldn't be needed
+                MenuContainer = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject;  //Shouldn't be needed
             }
+
+            mMenu = MenuContainer.transform.Find("MenuNotification").gameObject;
+            mSettingsPanel = MenuContainer.transform.Find("SettingsPanel").gameObject;
             //Create button
             mSettingsPanelTransform = mSettingsPanel.transform;
             TalkMode = mSettingsPanelTransform.Find("MicSettings").gameObject.transform.Find("TalkMode").gameObject;
-            TalkModeTransform = TalkMode.transform;
 
             mButton = GameObject.Instantiate(TalkMode);
             mButton.name = "KillBindButton";
@@ -78,34 +80,38 @@ namespace KillBind.Patches
         public static void CreateInScene()
         {
             if (!ExistsInMemory) { CreateInMemory(); return; } //If it isn't in memory, create one in memory
-
-            sceneButton = Object.Instantiate(mButton);
-            sceneButtonTransform = sceneButton.transform;
-
             if (IsInMainMenu())
             {
-                //1051.631 627.0407 -35.4911 (MicSettings)
-                //1105.000 634.1907 -35.4911 (Position I want the button to be)
-                currentOffset = MainMenuPositionOffset;
-                sceneSettingsPanel = GameObject.Find("Canvas").gameObject.transform.Find("MenuContainer").gameObject.transform.Find("SettingsPanel").gameObject;
+                MenuContainer = GameObject.Find("Canvas").gameObject.transform.Find("MenuContainer").gameObject;
             }
             else
             {
-                sceneSettingsPanel = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject.transform.Find("SettingsPanel").gameObject;
-                currentOffset = QuickMenuPositionOffset;
+                MenuContainer = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject;
             }
+
+            sceneSettingsPanel = MenuContainer.transform.Find("SettingsPanel").gameObject;
             sceneSettingsPanelTransform = sceneSettingsPanel.transform;
 
             TalkMode = sceneSettingsPanelTransform.Find("MicSettings").gameObject.transform.Find("TalkMode").gameObject;
-            TalkModeTransform = TalkMode.transform;
 
+            //Button code
+            sceneButton = Object.Instantiate(mButton);
+            sceneButtonTransform = sceneButton.transform;
             sceneButtonTransform.SetParent(sceneSettingsPanelTransform);
             sceneButtonTransform.SetAsFirstSibling(); //Avoid overlapping
-            sceneButtonTransform.localPosition = new Vector3(9.0871f, 235.4723f, -4.7728f);
+            sceneButtonTransform.localPosition = ButtonLocalPosition;
             sceneButtonTransform.rotation = new Quaternion(0, 0, 0, 0);
-            sceneButtonTransform.localScale = new Vector3(1f, 1f, 1f);
+            sceneButtonTransform.localScale = Vector3.one;
+            sceneButton.GetComponent<Button>().onClick.AddListener(delegate { onButtonClicked(); });
+
+            //Menu code
 
             return;
+        }
+
+        private static void onButtonClicked()
+        {
+            modLogger.LogInfo("the button was clicked c:");
         }
 
         private static bool IsInMainMenu()
