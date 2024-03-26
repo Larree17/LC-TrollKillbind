88722341bd6e61b5fb782b489d4541ebe722dc1c
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-26 21:37:55 +0100

    UI created, still needs dropdowns

--- KillBind/Patches/MenuManagerPatch.cs
+++ KillBind/Patches/MenuManagerPatch.cs
@@ -6,11 +6,14 @@ namespace KillBind.Patches
     [HarmonyPatch(typeof(MenuManager))]
     public class MenuManagerPatch
     {
+        public static MenuManager MenuManagerInstance;
+
         [HarmonyPatch("Awake")]
         [HarmonyPrefix]
         public static void OnAwake(MenuManager __instance)
         {
             if (__instance.isInitScene) { return; } //To avoid issues
+            MenuManagerInstance = __instance;
             CreateInMemory();
             CreateInScene();
         }
--- KillBind/Patches/UIHandler.cs
+++ KillBind/Patches/UIHandler.cs
@@ -1,9 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static KillBind.Initialise;
 
 namespace KillBind.Patches
 {
@@ -15,6 +13,9 @@ namespace KillBind.Patches
         private static TextMeshProUGUI mButtonTextComp;
 
         private static GameObject mMenu;
+        private static GameObject mMenuPanel;
+        private static GameObject mMenuButton;
+        private static GameObject mMenuText;
 
         private static GameObject mSettingsPanel;
         private static Transform mSettingsPanelTransform;
@@ -30,10 +31,13 @@ namespace KillBind.Patches
 
         private static GameObject sceneButton;
         private static Transform sceneButtonTransform;
+
         private static GameObject sceneSettingsPanel;
         private static Transform sceneSettingsPanelTransform;
+
         private static GameObject sceneMenu;
         private static Transform sceneMenuTransform;
+        private static GameObject sceneMenuButton;
 
         private static Vector3 ButtonLocalPosition = new Vector3(9.0871f, 235.4723f, -4.7728f);
 
@@ -77,6 +81,20 @@ namespace KillBind.Patches
             mMenu = GameObject.Instantiate(mMenu);
             mMenu.name = "KillBindMenu";
 
+            mMenuPanel = mMenu.transform.Find("Panel").gameObject;
+
+            mMenuButton = mMenuPanel.transform.Find("ResponseButton").gameObject;
+
+            GameObject.DestroyImmediate(mMenuButton.GetComponent<Button>()); //remove persistent listeners
+            mMenuButton.AddComponent<Button>();
+            mMenuButton.GetComponent<Button>().transition = Selectable.Transition.Animation; //rebind animation to button
+            mMenuButton.transform.localPosition = new Vector3(0, -70f, 1.3f);
+
+            mMenuText = mMenuPanel.transform.Find("NotificationText").gameObject;
+
+            mMenuText.GetComponent<TextMeshProUGUI>().text = "Kill Bind Settings";
+            mMenuText.transform.localPosition = new Vector3(0, 80f, -3f);
+
             //Store all in memory
             Object.DontDestroyOnLoad(mButton);
             Object.DontDestroyOnLoad(mMenu);
@@ -103,29 +121,40 @@ namespace KillBind.Patches
 
             //Button code
             sceneButton = Object.Instantiate(mButton);
+
             sceneButtonTransform = sceneButton.transform;
             sceneButtonTransform.SetParent(sceneSettingsPanelTransform);
             sceneButtonTransform.SetAsFirstSibling(); //Avoid overlapping
             sceneButtonTransform.localPosition = ButtonLocalPosition;
             sceneButtonTransform.rotation = new Quaternion(0, 0, 0, 0);
             sceneButtonTransform.localScale = Vector3.one;
-            sceneButton.GetComponent<Button>().onClick.AddListener(delegate { onButtonClicked(); });
+            //modLogger.LogInfo("Created button");
 
             //Menu code
             sceneMenu = Object.Instantiate(mMenu);
+            sceneMenu.SetActive(false);
+
             sceneMenuTransform = sceneMenu.transform;
             sceneMenuTransform.SetParent(sceneSettingsPanelTransform.parent);
             sceneMenuTransform.SetAsLastSibling(); //Avoid overlapping
-            sceneMenuTransform.localPosition = sceneSettingsPanelTransform.parent.localPosition;
+            sceneMenuTransform.localPosition = sceneSettingsPanelTransform.parent.transform.localPosition;
             sceneMenuTransform.rotation = new Quaternion(0, 0, 0, 0);
             sceneMenuTransform.localScale = Vector3.one;
 
+            sceneMenuButton = sceneMenuTransform.Find("Panel").gameObject.transform.Find("ResponseButton").gameObject;
+            //modLogger.LogInfo("Created menu");
+
+            //Add listeners
+            sceneButton.GetComponent<Button>().onClick.AddListener(delegate { onButtonClicked(true); });
+            sceneMenuButton.GetComponent<Button>().onClick.AddListener(delegate { onButtonClicked(false); });
+            //modLogger.LogInfo("Added all listeners");
             return;
         }
 
-        private static void onButtonClicked()
+        private static void onButtonClicked(bool setActive)
         {
-            modLogger.LogInfo("the button was clicked c:");
+            MenuManagerPatch.MenuManagerInstance.PlayConfirmSFX();
+            sceneMenu.SetActive(setActive);
         }
 
         private static bool IsInMainMenu()
