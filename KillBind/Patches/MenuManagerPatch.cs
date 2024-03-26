7a099b1d4b256490554078a541ca591d361bbd1d
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-26 16:51:01 +0100

    actually add patches

---
+++ KillBind/Patches/MenuManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using static KillBind.Patches.UIHandler;
+
+namespace KillBind.Patches
+{
+    [HarmonyPatch(typeof(MenuManager))]
+    public class MenuManagerPatch
+    {
+        [HarmonyPatch("Awake")]
+        [HarmonyPrefix]
+        public static void OnAwake(MenuManager __instance)
+        {
+            if (__instance.isInitScene) { return; } //To avoid issues
+            CreateInMemory();
+            CreateInScene();
+        }
+    }
+}
---
+++ KillBind/Patches/QuickMenuManagerPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using static KillBind.Patches.UIHandler;
+
+namespace KillBind.Patches
+{
+    [HarmonyPatch(typeof(QuickMenuManager))]
+    public class QuickMenuManagerPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void OnStart()
+        {
+            CreateInMemory(); //if for some reason it ain't there
+            CreateInScene();
+        }
+    }
+}
