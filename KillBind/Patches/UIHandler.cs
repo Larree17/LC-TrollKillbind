3054d4839d02a11f2624caaa87134649497aecb5
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-26 17:42:31 +0100

    push progress

--- KillBind/Patches/UIHandler.cs
+++ KillBind/Patches/UIHandler.cs
@@ -21,6 +21,7 @@ namespace KillBind.Patches
 
         private static GameObject TalkMode;
         private static Transform TalkModeTransform;
+        private static Transform MicSettingsTransform;
 
         //Scene
 
@@ -28,7 +29,10 @@ namespace KillBind.Patches
         private static Transform sceneButtonTransform;
         private static GameObject sceneSettingsPanel;
 
-        private static Vector3 positionOffset;
+        private static Vector3 MainMenuPositionOffset = new Vector3(53.369f, 7.19f, 0);
+        private static Vector3 QuickMenuOffsetScale = new Vector3(0.05714f, 0.06875f, 0); //for easier changes (maybe do it better sometime soon, with screen res n stuff)
+        private static Vector3 QuickMenuPositionOffset = Vector3.Scale(MainMenuPositionOffset, QuickMenuOffsetScale);
+        private static Vector3 currentOffset;
 
         /* Copy TalkMode (put in SettingsPanel)
          * Remove SettingsOption
@@ -50,15 +54,15 @@ namespace KillBind.Patches
                 mSettingsPanel = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject.transform.Find("SettingsPanel").gameObject; //Shouldn't be needed
             }
             //Create button
-            TalkMode = mSettingsPanel.transform.Find("MicSettings").gameObject.transform.Find("TalkMode").gameObject;
+            MicSettingsTransform = mSettingsPanel.transform.Find("MicSettings").gameObject.transform;
+            TalkMode = MicSettingsTransform.Find("TalkMode").gameObject;
             TalkModeTransform = TalkMode.transform;
 
             mButton = GameObject.Instantiate(TalkMode);
             mButton.name = "KillBindButton";
-            GameObject.Destroy(mButton.GetComponent<SettingsOption>());
+            GameObject.DestroyImmediate(mButton.GetComponent<SettingsOption>());
 
             mButtonTransform = mButton.transform;
-            mButtonTransform.position = TalkModeTransform.position;
 
             mButtonTextComp = mButtonTransform.Find("Text").gameObject.transform.GetComponent<TextMeshProUGUI>();
             mButtonTextComp.text = "Kill Bind Settings";
@@ -78,24 +82,26 @@ namespace KillBind.Patches
 
             sceneButton = Object.Instantiate(mButton);
             sceneButtonTransform = sceneButton.transform;
-
             if (IsInMainMenu())
             {
+                //1051.631 627.0407 -35.4911 (MicSettings)
+                //1105.000 634.1907 -35.4911 (Position I want the button to be)
+                currentOffset = MainMenuPositionOffset;
                 sceneSettingsPanel = GameObject.Find("Canvas").gameObject.transform.Find("MenuContainer").gameObject.transform.Find("SettingsPanel").gameObject;
-                positionOffset = new Vector3(36f, 14f, 0f);
             }
             else
             {
                 sceneSettingsPanel = GameObject.Find("Systems").gameObject.transform.Find("UI").gameObject.transform.Find("Canvas").gameObject.transform.Find("QuickMenu").gameObject.transform.Find("SettingsPanel").gameObject;
-                positionOffset = new Vector3(2f, 0.9625f, 0f);
+                currentOffset = QuickMenuPositionOffset;
             }
-
-            TalkMode = sceneSettingsPanel.transform.Find("MicSettings").gameObject.transform.Find("TalkMode").gameObject;
+            MicSettingsTransform = mSettingsPanel.transform.Find("MicSettings").gameObject.transform;
+            TalkMode = MicSettingsTransform.Find("TalkMode").gameObject;
             TalkModeTransform = TalkMode.transform;
 
             sceneButtonTransform.SetParent(sceneSettingsPanel.transform);
             sceneButtonTransform.SetAsFirstSibling(); //Avoid overlapping
-            sceneButtonTransform.position = TalkModeTransform.position + positionOffset;
+            sceneButtonTransform.position = MicSettingsTransform.position + currentOffset;
+            sceneButtonTransform.rotation = new Quaternion(0, 0, 0, 0);
             sceneButtonTransform.localScale = new Vector3(1f, 1f, 1f);
 
             return;
