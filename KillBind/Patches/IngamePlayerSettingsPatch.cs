a6ce52dbab06814fd4f785530e0ba3f606187a4d
Confusified <rogier.hendriksen2005@gmail.com>
2024-03-27 22:39:01 +0100

    saving settings works, still needs to be added: sound when in a lobby, visual cue that the settings have been changed

--- KillBind/Patches/IngamePlayerSettingsPatch.cs
+++ KillBind/Patches/IngamePlayerSettingsPatch.cs
@@ -1,6 +1,32 @@
-﻿namespace KillBind.Patches
+﻿using HarmonyLib;
+using static KillBind.Initialise;
+using static KillBind.Patches.UIHandler;
+
+namespace KillBind.Patches
 {
+    [HarmonyPatch(typeof(IngamePlayerSettings))]
     public class IngamePlayerSettingsPatch
     {
+        [HarmonyPatch("SaveChangedSettings")]
+        [HarmonyPostfix]
+        private static void OnSaveChanges()
+        {
+            ModSettings.DeathCause.Value = UnsetDeathCause;
+            ModSettings.HeadType.Value = UnsetHeadType;
+            return;
+        }
+
+        [HarmonyPatch("DiscardChangedSettings")]
+        [HarmonyPostfix]
+        private static void OnRestoreChanges()
+        {
+            DeathDropdownComponent.SetValueWithoutNotify(ModSettings.DeathCause.Value);
+            HeadDropdownComponent.SetValueWithoutNotify(ModSettings.HeadType.Value);
+
+            UnsetDeathCause = ModSettings.DeathCause.Value;
+            UnsetHeadType = ModSettings.HeadType.Value;
+
+            return;
+        }
     }
 }
