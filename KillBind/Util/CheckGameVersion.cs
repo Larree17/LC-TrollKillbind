5449032c7c2ebcf404d1b1a8682daac9f9b8d078
Confusified <60018584+Confusified@users.noreply.github.com>
2024-04-03 20:54:55 +0200

    add back legacy code stuff and other stuff idk but it's v50 compatible now

---
+++ KillBind/Util/CheckGameVersion.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace KillBind.Util
+{
+    public class CheckGameVersion
+    {
+        private const string v49Hash = "";
+        private const string v47Hash = "";
+        private static string currentHash = Assembly.GetEntryAssembly().ManifestModule.ModuleVersionId.ToString();
+
+        public static bool Isv50()
+        {
+            if (v49Hash == currentHash || v47Hash == currentHash) { return true; }
+            return false;
+        }
+    }
+}
